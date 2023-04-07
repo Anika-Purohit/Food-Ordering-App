0b8170b58335cb8ec320337682078720e3cf6d6e
Anika-Purohit <anikapurohit6@gmail.com>
2023-04-07 19:49:28 +0530

    API issue solved

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,73 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IMG_CDN } from "./Config";
 import useRestaurant from "../utils/useRestaurant";
-import { img_cdn } from "./Config";
-import { Shimmer } from "./Shimmer";
-import useRestaurant from "../utils/useRestaurant";
-const RestaurantMenu = () =>{
-    const {id} = useParams();
-    const restaurant = useRestaurant();    
-    return (!restaurant)?(<Shimmer/>):
-    (
-        <>
-        <div className = 'eachRestaurantInfo'>
-        <h1>Restaurant id : {id} </h1>
-        <img src ={img_cdn+restaurant.cloudinaryImageId} width='300px'/>
-        <h2>{restaurant.name}</h2>
-        <h3>Area: {restaurant.areaName} </h3>
-        <h3>Average Rating: {restaurant.avgRating} Stars</h3>
-        <h3>Cost: {restaurant.costForTwoMessage}</h3>
-       
-        </div>
-        </>
-    )    
-    }
-    export default RestaurantMenu
+import {Shimmer} from "./Shimmer";
+// import { Header } from "./Header";
+import Footer from "./Footer";
+
+const RestaurantMenu = () => {
+  const { id} = useParams();
+
+  const restaurant = useRestaurant(id);
+
+  const recData =
+    restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.find(
+      (items) => items.card.card.title == "Recommended"
+    );
+
+  console.log(recData);
+
+  // .find(items => items.card.card.title=="Recommended")
+
+  return !restaurant ? (<Shimmer />) :(
+          <>
+          <div className="menu-items-container">
+            <div className="menu-title-wrap">
+              <h3 className="text-2xl font-semibold">BEST SELLERS</h3>
+              <p className="text-xl ">
+                {Object.keys(recData?.card?.card?.itemCards).length} ITEMS
+              </p>
+            </div>
+            
+                  
+            <div className="menu-items-list">
+              {Object.values(recData?.card?.card?.itemCards).map(
+                (itemCards) => (
+                  
+                  <div className="flex">
+                    <div className="menu-img-wrapper">
+                      {itemCards?.card?.info?.imageId && (
+                        <img
+                          className="w-52"
+                          src={IMG_CDN + itemCards?.card?.info?.imageId}
+                          alt={itemCards?.card?.info?.description}
+                        />
+                      )}
+                      <button className="btn bg-orange-400 add-btn">
+                      </button>
+                    </div>
+                    <div className="flex">
+                      <h3 className="font-bold m-3 flex">
+                        {itemCards?.card?.info?.name}
+                      </h3>
+                      <p className="flex m-3 ">
+                        {itemCards?.card?.info?.price > 0
+                          ? new Intl.NumberFormat("en-IN", {
+                              style: "currency",
+                              currency: "INR",
+                            }).format(itemCards?.card?.info?.price / 100)
+                          : " "}
+                      </p>
+                      </div>
+                  </div>
+                )
+              )}
+            </div>
+          </div>
+    
+    </>
+  );
+}
+
+export default RestaurantMenu;
--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -1,18 +1,17 @@
-    import { useEffect,useState } from "react";
+import { useEffect,useState } from "react";
    
-    const useRestaurant = () => {
-    const[restaurant,setRestaurant] = useState({});
+    const useRestaurant = (id) => {
+    const[restaurant,setRestaurant] = useState(null);
   
     useEffect(()=>{getRestaurantInfo();} ,[] );
 
-    async function getRestaurantInfo(){
+    async function getRestaurantInfo() {
     const data = await fetch(
-    "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.385044&lng=78.486671&restaurantId="+id+"&submitAction=ENTER"
-    ) ;
-    const json = await data.json();
-    console.log(json.data);  
-    setRestaurant(json?.data?.cards[0]?.card?.card?.info); 
-    }
+     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.5270362&lng=77.13593279999999&restaurantId="+id);
+            const json = await data.json();
+            console.log(json.data);
+            setRestaurant(json.data);
+          }
     return restaurant;
 };
 export default useRestaurant;
