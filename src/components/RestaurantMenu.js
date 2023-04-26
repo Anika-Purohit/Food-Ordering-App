97fc05c9b0b356aa84e06ffb4d479a7666689d62
Anika-Purohit <anikapurohit6@gmail.com>
2023-04-26 08:21:20 +0530

    Menu api problem solved

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,93 +1,111 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN } from "./Config";
 import useRestaurant from "../utils/useRestaurant";
-import {Shimmer} from "./Shimmer";
+import { Shimmer } from "./Shimmer";
 import { useDispatch } from "react-redux";
 import { addItem } from "../utils/cartSlice";
-import { removeItem} from "../utils/cartSlice";
-
+import { removeItem } from "../utils/cartSlice";
 
 const RestaurantMenu = () => {
-  const { id} = useParams();
+  const { id } = useParams();
 
   const restaurant = useRestaurant(id);
+  const restaurantInfo = restaurant?.cards[0]?.card?.card?.info;
+  const restaurantMenuInfo = restaurant?.cards;
 
   const dispatch = useDispatch();
-
-  const handleAddItem = (itemCards) =>{
+  const handleAddItem = (itemCards) => {
     dispatch(addItem(itemCards));
   };
-  const handleRemoveItem = (itemCards) =>{
+  const handleRemoveItem = (itemCards) => {
     dispatch(removeItem(itemCards));
   };
-  
-  const recData =
-    restaurant?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.find(
-      (items) => items?.card?.card?.title == "Recommended"
-    );
 
-  console.log(recData);
+  let result = [],
+    uniqueFoodItems = [];
 
-  // .find(items => items.card.card.title=="Recommended")
+  const customFilter = (object, result) => {
+    if (object.hasOwnProperty("itemCards")) result.push(object);
 
-  return !recData ? (<Shimmer />) :(
-          <>
-          <div data-testid="menu" className="bg-yellow-50">
-            <div className="ml-5 text-red-900">
-              <h3 className="text-2xl font-bold ">Best Sellers:</h3>
-              <p className="text-xl font-mono">
-                {Object?.keys(recData?.card?.card?.itemCards).length} Items
-              </p>
-            </div>
-            
-                  
-            <div className="">
-              {Object.values(recData?.card?.card?.itemCards).map(
-                (itemCards) => (
-                  
-                  <div className="inline-block w-52 m-5 h-32px bg-white rounded-2xl align-top ">
-                    <div className="menu-img-wrapper">
-                      {itemCards?.card?.info?.imageId && (
-                        <img
-                          className="w-52 shadow-2xl rounded-2xl border-8 border-white"
-                          src={IMG_CDN + itemCards?.card?.info?.imageId}
-                          alt={itemCards?.card?.info?.description}
-                        />
-                      )}
-                     
-                    </div>
-                    <div className="text-orange-900 grid grid-rows-4">             {/*grid container*/}
-                       <div class="row-span-2">                                                          {/* div in grid #1 */}
-                         <h3 className="font-bold text-lg m-3 flex text-red-900 ">
-                         {itemCards?.card?.info?.name}
-                         </h3>
-                       </div>
-                      
-                       <div className="row-span-1">
-                         <p className=" m-3  text-lg text-red-90 font-mono">
-                          {itemCards?.card?.info?.price > 0 ?new Intl.NumberFormat("en-IN", {
-                              style: "currency",
-                              currency: "INR",
-                             }).format(itemCards?.card?.info?.price / 100): "Not Mentioned"}
-                         </p>
-                       </div>
-                       <div className="mb-auto">
-                         <button data-testid="addBtn" className = "bg-orange-900  font-mono w-20 ml-2 text-white rounded-full h-9"onClick={()=>handleAddItem(itemCards?.card?.info)}>
-                         Add</button>
-                         <button className = "bg-orange-900 w-24 ml-2  font-mono text-white rounded-full  h-9"onClick={()=>handleRemoveItem(itemCards?.card?.info)}>
-                         Remove</button> 
-                       
-                       </div>
-                    </div>
-                  </div>
-                )
-              )}
-            </div>
-          </div>
-    
-    </>
+    for (var i = 0; i < Object.keys(object).length; i++) {
+      if (typeof object[Object.keys(object)[i]] == "object") {
+        customFilter(object[Object.keys(object)[i]], result);
+      }
+    }
+  };
+
+  const resMenu =
+    restaurantMenuInfo?.length > 0 && customFilter(restaurantMenuInfo, result);
+
+  if (result.length > 0) {
+    const uniqueIds = [];
+    let uniqueItems = [
+      ...new Set(result.flatMap((f) => f.itemCards).map((p) => p.card.info)),
+    ];
+    uniqueFoodItems = uniqueItems?.filter((element) => {
+      const isDuplicate = uniqueIds.includes(element.id);
+
+      if (!isDuplicate) {
+        uniqueIds.push(element.id);
+
+        return true;
+      }
+
+      return false;
+    });
+  }
+
+  return (
+    <div>
+      {!restaurant ? (
+        <Shimmer />
+      ) : (
+        <div>
+          <h1>Menu</h1>
+          {uniqueFoodItems.length > 0 ? (
+            Object.values(uniqueFoodItems).map((item, index) => {
+              if (index < 25) {
+                return (
+                  <li key={index}>
+                    <>
+                      <div>
+                        <span>{item?.name}</span>
+                        <span>
+                          ₹ {(item?.price || item?.defaultPrice) / 100}
+                        </span>
+                      </div>
+                      <div>
+                        {item?.imageId && (
+                          <img src={IMG_CDN + item?.imageId} alt="item" />
+                        )}
+                        <div>
+                          <button
+                            onClick={() => {
+                              handleRemoveItem();
+                            }}
+                          >
+                            Remove
+                          </button>
+                          <button
+                            onClick={() => {
+                              handleAddItem();
+                            }}
+                          >
+                            Add
+                          </button>
+                        </div>
+                      </div>
+                    </>
+                  </li>
+                );
+              }
+            })
+          ) : (
+            <span>No restaurant menu items.</span>
+          )}
+        </div>
+      )}
+    </div>
   );
-}
-
+};
 export default RestaurantMenu;
