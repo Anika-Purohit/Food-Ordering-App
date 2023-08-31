8088da556a1100191279ca9d41c72ece8d8cc93a
Anika-Purohit <anikapurohit6@gmail.com>
2023-08-31 14:05:38 +0530

    ,,

--- src/components/config.js
+++ src/components/config.js
@@ -1,4 +1,4 @@
 export const IMG_CDN = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
-export const RESTAURANT_CDN = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.385044&lng=78.486671&offset=15&sortBy=RELEVANCE&pageType=SEE_ALL&page_type=DESKTOP_SEE_ALL_LISTING";
+export const RESTAURANT_CDN = "https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5270362&lng=77.13593279999999&page_type=DESKTOP_WEB_LISTING";
 export const SEARCH_ICON="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSJDpLFa4ttUA3iEn2Nt_30SsqWbQ1BJou3BfFHaTyU8MA3Okc9_leWuV3AjyRjkzOd6E&usqp=CAU";
